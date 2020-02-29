233e6861b3421384742996b313be8b4e32b932c0
zaenal-ar <arifzaenal1945@gmail.com>
2020-02-29 17:46:28 +0700

    remove

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -72,7 +72,7 @@
 
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; {{ date("Y") }} <div class="bullet"></div> By <a href="">BPKAD</a>
+          
         </div>
       </footer>
     </div>
